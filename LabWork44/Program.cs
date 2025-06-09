39f2a50c6956a20e17db25cd16d3fbd61a345755
0109-04 <0109-04@0109-04>
2025-06-09 12:20:05 +0300

    Р”РѕР±Р°РІСЊС‚Рµ С„Р°Р№Р»С‹ РїСЂРѕРµРєС‚Р°.

---
+++ LabWork44/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
+using System.Diagnostics;
+
+CreateDatabase("test");
+
+
+void CreateDatabase(string name)
+{
+    string file = Path.Combine(Environment.CurrentDirectory, $"{name}.sqlite");
+    if (File.Exists(file))
+    {
+        Console.WriteLine("БД существует");
+        return;
+    }
+
+    SqliteConnectionStringBuilder builder = new();
+    builder.DataSource = file;
+    string connectionString = builder.ConnectionString;
+
+    using SqliteConnection connection = new(connectionString);
+    connection.Open();
+    Console.WriteLine("БД создано");
+
+    string query = @"CREATE TABLE Game(
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Title TEXT NOT NULL,
+                    Description TEXT NOT NULL,
+                    PublicationYear INTEGER NOT NULL,
+                    Price REAL NOT NULL
+                    )";
+    SqliteCommand command = new(query, connection);
+    command.ExecuteNonQuery();
+
+    query = @"CREATE TABLE Review (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            GameId INTEGER NOT NULL, User TEXT NOT NULL,
+            Comment TEXT NOT NULL,
+            PublicationDate TEXT NOT NULL,
+            FOREIGN KEY (GameId) REFERENCES Game(Id))";
+    command = new(query, connection);
+    command.ExecuteNonQuery();
+
+    Console.WriteLine("Таблицы созданы");
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient;
+
+CreateDatabase();
+
+
+void CreateDatabase()
+{
+    SqlConnectionStringBuilder builder = new()
+    {
+        DataSource = "mssql",
+        InitialCatalog = "ispp3104",
+        UserID = "ispp3104",
+        Password = "3104",
+        TrustServerCertificate = true
+    };
+
+    string connectionString = builder.ConnectionString;
+
+    using SqlConnection connection = new(connectionString);
+    connection.Open();
+    Console.WriteLine("Подключено");
+
+    string query = @"CREATE TABLE Roles (
+                        Id INT IDENTITY(1,1) PRIMARY KEY,
+                        Name NVARCHAR(200) NOT NULL
+                        )";
+    SqlCommand command = new(query, connection);
+    command.ExecuteNonQuery();
+
+    query = @"CREATE TABLE Users (
+                Id INT IDENTITY(1,1) PRIMARY KEY,
+                RoleId INT NOT NULL,
+                Login NVARCHAR(50) NOT NULL,
+                Password NVARCHAR(30) NOT NULL,
+                FOREIGN KEY (RoleId) REFERENCES Roles(Id)
+                )";
+    command = new(query, connection);
+    command.ExecuteNonQuery();
+
+    Console.WriteLine("Таблицы созданы");
+}
